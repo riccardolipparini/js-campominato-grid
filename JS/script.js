2d41e9589f2eecbbd78d4e5bea58332328221b33
Riccardo <riccardolipparini0@gmail.com>
2021-11-02 15:47:40 +0100

    primo push

---
+++ JS/script.js
@@ -0,0 +1,17 @@
+// Consegna L’utente indica un livello di difficoltà in base al quale viene generata 
+// una griglia di gioco quadrata,
+// in cui ogni cella contiene un numero tra quelli compresi in un range:
+// con difficoltà 1 => tra 1 e 100
+// con difficoltà 2 => tra 1 e 81
+// con difficoltà 3 => tra 1 e 49
+// Quando l’utente clicca su ogni cella, la cella cliccata si colora di azzurro 
+
+
+
+// 1 chiedere all'utente a che difficoltà vuole giocare
+// 2 utilizzare una griglia in base alla difficoltà scelta
+// 3 dare la possibilità all'utente di evidenziare i quadrati toccati
+
+const mineField = document.getElementById("campominato");
+
+const chooseDif = prompt("A quale difficoltà vuoi giocare?")
